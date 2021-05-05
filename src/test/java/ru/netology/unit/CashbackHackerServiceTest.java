9e6eb2400f360ab9d71719b0403d7634549330ab
Aigul Biktasheva <meleuz13@gmail.com>
2021-05-05 20:57:25 +0500

    update build

---
+++ src/test/java/ru/netology/unit/CashbackHackerServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackerServiceTest {
+    CashbackHackerService service = new CashbackHackerService();
+
+    @Test
+    public void shouldAmountLessThan1000() {
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAmountMoreThan1000() {
+        int actual = service.remain(1100);
+        int expected = 900;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAmount1000() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+}
